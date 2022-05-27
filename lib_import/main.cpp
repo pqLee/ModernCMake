bc982956a3dbc5aff22808551e7b9b442fffbc66
Li Pengqiang <pengqiang.li@intel.com>
2022-05-27 13:53:00 +0800

    Modify lib deps and test the generated libs

--- lib_import/main.cpp
+++ lib_import/main.cpp
@@ -1,9 +1,13 @@
 #include "bar.h"
+#include "foo.h"
+#include "dep.h"
 #include <iostream>
 using namespace std;
 
 int main( int argc, char **argv )
 {
-	bar();
+	// std::cout << "dep() : " << dep() << std::endl;
+	// std::cout << "foo() : " << foo() << std::endl;
+	std::cout << "bar() : " << bar() << std::endl;
 	return 0;
 }
